42dcae649dd112771666afff0a57e88583e8d734
Zese07 <zeroseven110703@gmail.com>
2024-05-19 16:48:01 +0800

    U

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors()); // Use cors middleware to handle CORS headers
 app.use('/genre', require('./routes/genre'));
 app.use('/profile', require('./routes/profile'));
+
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'index.html'));

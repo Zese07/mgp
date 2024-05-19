5d3a2f6083ea3416ac9c7794b5cf61f2f5a593a2
Zese07 <zeroseven110703@gmail.com>
2024-05-19 16:43:32 +0800

    U

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors()); // Use cors middleware to handle CORS headers
 app.use('/genre', require('./routes/genre'));
 app.use('/profile', require('./routes/profile'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
